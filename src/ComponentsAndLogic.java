00febbc0a82e4b71f754ab75587ccfc0b0c370fb
Mark <marikgames12@gmail.com>
2023-07-17 17:46:53 +0300

    added: - Login works, it remains to add registration - Learned to manually change the position of elements in the window -

--- src/ComponentsAndLogic.java
+++ src/ComponentsAndLogic.java
@@ -1,2 +1,62 @@
-public class ComponentsAndLogic {
+import javax.sound.sampled.Line;
+import javax.swing.*;
+import java.util.LinkedList;
+
+
+public class ComponentsAndLogic extends JFrame {
+    private JLabel emailText, passwordText, textRight;
+    private JTextField emailField;
+    private JPasswordField jPasswordField;
+    private JButton Login;
+    private LinkedList<String> userInfo = new LinkedList<>();
+    public ComponentsAndLogic(){
+        super("LogIn");
+        setLayout(null);
+
+        userInfo.add("mark");
+        userInfo.add("123");
+        userInfo.add("Admin");
+        userInfo.add("AdminPass");
+
+
+        emailText = new JLabel("Email: ");
+        emailText.setBounds(10,25,80,10);
+
+        passwordText = new JLabel("Password: ");
+        passwordText.setBounds(10, 50, 80, 10 );
+
+        emailField = new JTextField(20);
+        emailField.setBounds(100,20,165,25);;
+
+        jPasswordField = new JPasswordField();
+        jPasswordField.setBounds(100,45,165,25);
+
+        Login = new JButton("Login");
+        Login.setBounds(30,90,80,25);
+
+        textRight = new JLabel("");
+        textRight.setBounds(130,90,300,25);
+
+        Login.addActionListener(e -> {
+            try {
+                String password, email;
+                password = jPasswordField.getText();
+                email = emailField.getText();
+                if (userInfo.contains(password) && userInfo.contains(email)) {
+                    textRight.setText("Hello " + email);
+                } else {
+                    textRight.setText("wrong password or email");
+                }
+            }catch (Exception ex){
+
+            }
+        });
+
+        add(emailText);
+        add(emailField);
+        add(jPasswordField);
+        add(passwordText);
+        add(Login);
+        add(textRight);
+    }
 }
--- src/MainLogWindow.java
+++ src/MainLogWindow.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class MainLogWindow {
     public static void main(String[] args) {
-
+           ComponentsAndLogic window = new ComponentsAndLogic();
+           window.setResizable(false);
+           window.setSize(290, 170);
+           window.setLocationRelativeTo(null);
+           window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+           window.setVisible(true);
     }
 }
